ba6749a65a2758ff65019ea85086cf6bc36ea072
Antony Davenport <tonydav43@hotmail.co.uk>
2022-05-22 11:11:03 +0200

    Change setting.py

--- recipes/settings.py
+++ recipes/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
-
 ENV_ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOST') or None
 ALLOWED_HOSTS = []
 if not DEBUG:
